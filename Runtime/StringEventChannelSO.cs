98400ecbfb59ee9eaa282335414c88c759d0a87c
cyhzz <1076357483@qq.com>
2024-01-11 03:07:34 +0800

    asd

--- Runtime/StringEventChannelSO.cs
+++ Runtime/StringEventChannelSO.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Scripting;
+
 namespace Com.A9.EventSO
 {
     [CreateAssetMenu(menuName = "Events/String Event Channel")]
+    [Preserve]
     public class StringEventChannelSO : ScriptableObject
     {
         public UnityAction<string> OnEventRaised;
